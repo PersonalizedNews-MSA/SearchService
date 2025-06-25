dd343d892e84947b7d0363fc2b8ee021b7c59872
wkdnffla3 <wkdnffla3@gmail.com>
2025-06-25 23:11:04 +0900

    chore. update

--- src/main/java/com/mini2/SearchService/common/exception/handler/FavoriteFeignClient.java
+++ src/main/java/com/mini2/SearchService/common/exception/handler/FavoriteFeignClient.java
@@ -6,6 +6,7 @@ import com.mini2.SearchService.dto.response.NewsResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -13,5 +14,6 @@ import java.util.List;
 @FeignClient(name = "favorite-service", url = "http://k8s-favorite-service:8080",configuration = FeignAuthConfig.class)
 public interface FavoriteFeignClient {
     @GetMapping("/api/favorite/v1/newslinks")
-    List<String> getFavoriteNewsLinks(Long userId);
+//    List<String> getFavoriteNewsLinks(Long userId);
+    List<String> getFavoriteNewsLinks(@RequestHeader("X-Auth-userId") Long userId);
 }
